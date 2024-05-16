946926192d97828570dac92d8b4818de5a11d56c
Aubrianna Sample <sample.aubry@gmail.com>
2024-05-16 14:46:25 -0700

    Initial Commit

---
+++ EvoHabit/EvoHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvoHabitApp.swift
+//  EvoHabit
+//
+//  Created by Aubrianna Sample on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct EvoHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
